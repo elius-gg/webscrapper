2bc3c670b1b2524a5e382fb7988ef5651ae19a06
eliusgg <elius.gutgall@gmail.com>
2022-06-07 21:30:16 -0400

    Version inicial con esqueleto del programa

---
+++ main.py
@@ -0,0 +1,23 @@
+import argparse
+import logging
+logging.basicConfig(level=logging.INFO)
+
+from common import config
+
+logger = logging.getLogger(__name__)
+
+
+def _news_scraper(news_site_uid):
+    host = config()['news_sites'][news_site_uid]['url']
+
+    logger.info(f'Beginning scraper for {host}')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    news_site_choices = list(config()['news_sites'].keys())
+    parser.add_argument('news_site', help='El sitio a scrapear', type=str, choices=news_site_choices)
+
+
+    args = parser.parse_args()
+    _news_scraper(args.news_site)
